8366d3e0d876fa0e6f242408fcfa6c40fd6aef01
John Jensen MBP <johncjensen@hotmail.com>
2014-02-03 13:37:31 -0800

    add test folder and basic setup

---
+++ test/features/visitor/submit_anon_story_test.rb
@@ -0,0 +1,6 @@
+require "test_helper"
+
+feature "Submit anonymous story" do
+  scenario "so that people can revel in the craziness that happened one night/day" do
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,14 @@
+ENV["RAILS_ENV"] = "test"
+require File.expand_path("../../config/environment", __FILE__)
+require "rails/test_help"
+require "minitest/rails/capybara"
+
+class ActiveSupport::TestCase
+  fixtures :all
+end
+
+class ActionDispatch::IntegrationTest
+  include Rails.application.routes.url_helpers
+  include Capybara::DSL
+end
+
