b5a2fe6e2d62c449b4b092e8ade912756e2a9160
Jonah Kirangi <jonahkirangi@gmail.com>
2014-02-05 13:10:22 -0800

    Add title length validation to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
 
   validates_inclusion_of :category, :in => [ "general", "nightclub", "workplace", "school", "relationships", "hookups" ]
   self.per_page = 20
-  validates :title, :presence => {:message => 'Title cannot be blank'}
+  validates :title, :presence => {:message => 'Title cannot be blank'}, :length => {:maximum => 78}
   validates :body, :presence => {:message => 'Body cannot be blank'}
 end
