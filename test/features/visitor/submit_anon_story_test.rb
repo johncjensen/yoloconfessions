bd827ea8b084504bcfb36870277f88c59211194f
Jonah Kirangi <jonahkirangi@gmail.com>
2014-02-06 15:48:29 -0800

    Update nav bar, submit story test, and add admin info section

--- test/features/visitor/submit_anon_story_test.rb
+++ test/features/visitor/submit_anon_story_test.rb
@@ -11,8 +11,8 @@ feature "Submit anonymous story" do
     fill_in "Body", with: "Yolo ipsum dolor sit amet, consectetur adipiscing elit. Ut ac suscipit leo. Carpe diem vulputate est nec commodo rutrum. Pellentesque mattis convallis nisi eu and I ain’t stoppin until the swear jar’s full. Ut rhoncus velit at mauris interdum, fringilla dictum neque rutrum. Curabitur mattis odio at erat viverra lobortis. Poppin’ bottles on the ice, tristique suscipit mauris elementum tempus. Quisque ut felis vitae elit tempor interdum viverra a est. Drop it like it’s hot, at pretium quam. In nec scelerisque purus. Nam dignissim lacus ipsum, a ullamcorper nulla pretium non. Aliquam sed enim faucibus, pulvinar felis at, vulputate augue. Ten, ten, twenties on them fifties, trick, at tempus libero fermentum id. Vivamus ut nisi dignissim, condimentum urna vel, dictum massa. Donec justo yolo, rutrum vitae dui in, dapibus tempor tellus. I do it big. Fusce ut sagittis mi."
 
     select('nightclub', :from => 'post_category')
-    click_on "Create Post"
-    save_and_open_page
-    # page.text.must_include "Drop it like it’s hot"
+    click_on "Submit"
+
+    page.text.must_include "Thanks for your submission!"
   end
 end
