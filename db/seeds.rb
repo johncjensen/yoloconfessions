67e0aa92590f7962a13320baeb8049f1612e6ba5
John Jensen MBP <johncjensen@hotmail.com>
2014-02-06 19:42:25 -0800

    add approve button to admin panel

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-admin = User.create(email: "admin@admin.com", password: "password", password_confirmation: "password")
+admin = User.create(email: "admin@admin.com", password: "password1!", password_confirmation: "password1!")
 
 
 require 'ffaker'
